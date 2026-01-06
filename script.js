0520a7891fe4cbe98700bc98c43e4bba1be51060
RPQ35 <alfajriropiq@gmail.com>
2026-01-06 15:50:02 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function showPage(pageNumber) {
+    const items = document.querySelectorAll('.obj');
+    const itemsPerPage = 8;
+    
+    // Calculate our bounds
+    const start = (pageNumber - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+  
+    items.forEach((item, index) => {
+      // If the index is between our start and end, show it; otherwise, hide it
+      if (index >= start && index < end) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  }
+  
+  // Run once on load to show the first page
+  showPage(1);
